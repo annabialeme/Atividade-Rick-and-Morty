413d3eb700874ba2ce939c4b54c6af7ae0977c59
anna leme <annaleme.tds1.senai@gmail.com>
2025-04-17 10:48:59 -0300

    feat: page fetch and button

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -4,26 +4,47 @@ import { useEffect, useState } from "react";
 import CharacterCard from "../../components/CharacterCard";
 import styles from "../home/Home.module.css"
 import axios from "axios";
+
+
 export default function Home() {
+    const [search, setSearch] = useState("");
     const [characters, setCharacters] = useState([]);
+    const [notFound, setNotFound] = useState(false);
+
+
+
+    const fetchCharacters = async (name = "") => {
+        setNotFound(false);
+        try {
+            const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?name=${name}`);
+            setCharacters(data.results);
+        } catch {
+            setCharacters([]);
+            setNotFound(true);
+        }
+    }
     
     useEffect(() => {
-        axios.get("https://rickandmortyapi.com/api/character")
-        .then((response) => {
-            setCharacters(response.data.results);
-        })
-        .catch((error) => {
-            console.error("Erro ao buscar personagens:", error);
-        });  
-    }, []);  
-    
-console.log(characters);
+        fetchCharacters();
+    } 
+);
 
 return (
     <div className={styles.container} >
+        <h1 className={styles.title}>Personagens de Rick and Morty</h1>
+        <div className={styles.controls}>
+            <input type = "text" placeholder="Buscar por nome" value={search} onChange={(e) => setSearch(e.target.value)} className={styles.input} />
+
+            <button onClick={()  => fetchCharacters(search.trim())} className={styles.buttonSearch}>Buscar</button>
+
+            <button onClick={() => {
+                setSearch("");
+                fetchCharacters();
+            }} className={styles.buttonReset}>Resetar</button>
+        </div>
         <div className={styles.grid}>
-       {characters.map((character) => (
-            <CharacterCard key={character.id} character={character} />
+       {characters.map((char) => (
+            <CharacterCard key={char.id} character={char} />
         ))}
     </div>
     </div>
